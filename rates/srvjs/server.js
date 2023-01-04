3d9792f6ee2caf4eb43da30421e583a5e73216f8
Andrew Lunde <andrew.lunde@sap.com>
2023-01-04 17:26:05 -0500

    need router setup

--- rates/srvjs/server.js
+++ rates/srvjs/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 const xsenv = require('@sap/xsenv');
+// xsenv.loadEnv("default-env-good.json");
+// xsenv.loadEnv("default-env-bad.json");
 xsenv.loadEnv();
 const services = xsenv.getServices({
-    uaa: { label: 'xsuaa' },
-    registry: { label: 'saas-registry' },
-    dest: { label: 'destination' }
+    uaa: { label: 'xsuaa' } //,
+    // registry: { label: 'saas-registry' },
+    // dest: { label: 'destination' }
 });
 
 // const httpClient = require('@sap-cloud-sdk/http-client');
@@ -37,6 +39,24 @@ app.get('/srvjs/info', function (req, res) {
         res.status(403).send('Forbidden');
     }
 });
+
+// app registration
+app.get('/srvjs/reg', function (req, res) {
+    console.log('srvjs registration');
+    if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+        let info = {
+            'userInfo': req.user,
+            'subdomain': req.authInfo.getSubdomain(),
+            'tenantId': req.authInfo.getZoneId()
+        };
+        res.status(200).json(info);
+    } else {
+        res.status(403).send('Forbidden');
+    }
+});
+
+//Setup Routes
+// var router = require("./router")(app, server);
 
 const port = process.env.PORT || 5002;
 app.listen(port, function () {
